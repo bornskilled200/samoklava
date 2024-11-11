d58af809c9dcc1c35fd0d10e2633b44a3627c77c
David Park <davidpark4393@gmail.com>
2024-11-11 16:39:34 +0700

    incremental building

--- build.js
+++ build.js
@@ -10,12 +10,16 @@ const { stdin: input, stdout: output } = require('process');
 const ergogenCli = path.resolve(require.resolve('ergogen'), '../cli.js');
 const openjscadCli = path.resolve(require.resolve('@jscad/openjscad'), '../cli/cli.js');
 
-const generatePcbImage = spawnPcbImageProcess();
+const [generatePcbImage, generateGerber] = spawnPcbImageProcess();
+const [exportDsn, importSes] = spawnConvertKicadPcbToDSNProcess();
+const freeRouting = freeRoutingProcess();
 
 const generate = (async () => {
   fs.rm('output/pcbs/board.dsn', { force: true });
   fs.rm('output/routed_pcbs/board.ses', { force: true });
   fs.rm('output/routed_pcbs/board.kicad_pcb', { force: true });
+  fs.mkdir('output/routed_pcbs', { recursive: true });
+  fs.mkdir('output/gerbers', { recursive: true });
   console.log('building...');
   await once(fork(ergogenCli, ['-d', '.'], {}), 'close');
 
@@ -23,13 +27,17 @@ const generate = (async () => {
     ...['case_stl', 'base_stl', 'plate_stl']
       .map(file => once(fork(openjscadCli, [`output/cases/${file}.jscad`, '-o', `output/cases/${file}.stl`], {}), 'close')),
     generatePcbImage('pcbs/board'),
-    routePcb(generatePcbImage),
+    routePcb(),
   ]);
 });
 generate();
 
 process.on('exit', () => {
   console.log('exiting');
+});
+process.on('SIGINT', () => {
+  console.log('exiting');
+  process.exit();
 });
 
 const rl = readline.createInterface({ input, output });
@@ -38,6 +46,10 @@ rl.on('line', (input) => {
     process.exit();
   }
   generate();
+});
+
+rl.on("SIGINT", function () {
+  process.emit("SIGINT");
 });
 
 async function exists(...files) {
@@ -57,12 +69,24 @@ async function retry(callback, ...files) {
     }
     await callback();
     retryCount++;
-  } while(!exists(files) && retryCount < 3);
+  } while(await exists(...files) === false);
 }
 
-function spawnPcbImageProcess() {
-  console.log('spawning pcb imager');
-  const spawnMessage = 'pcbImageProcess spawned';
+async function onceMessage(process, message) {
+  return new Promise(resolve => {
+    const listener = (data) => {
+      if (data.includes(message)) {
+        resolve();
+        process.stdout.off('data', listener);
+      }
+    };
+    process.stdout.on('data', listener);
+  });
+}
+
+async function spawnDockerImage(identifier, image) {
+  console.log(`spawning ${identifier} docker image`);
+  const spawnMessage = `${identifier} spawned`;
   const pcbImageProcess = spawn('docker', [
     'run',
     '-i',
@@ -70,44 +94,36 @@ function spawnPcbImageProcess() {
     `-v ${__dirname}:/board`,
     '--rm',
     '--entrypoint', '/bin/bash',
-    'yaqwsx/kikit:v1.3.0',
+    image,
     '-c',
     `"echo ${spawnMessage}; /bin/bash"`,
   ], { shell: true });
-  const spawnedPromise = new Promise(resolve => {
-    const listener = (data) => {
-      if (data.includes(spawnMessage)) {
-        console.log('spawned pcb imager');
-        resolve();
-        pcbImageProcess.stdout.off('data', listener);
-      }
-    };
-    pcbImageProcess.stdout.on('data', listener);
-  });
+  const spawnedPromise = onceMessage(pcbImageProcess, spawnMessage);
 
-  pcbImageProcess.stdout.on('data', (data) => {
-    // console.log(`stdout: ${data}`);
-  });
-  pcbImageProcess.stderr.on('data', (data) => {
-    // console.error(`stderr: ${data}`);
-  });
+  // pcbImageProcess.stdout.on('data', (data) => {
+  //   console.log(`stdout: ${data}`);
+  // });
+  // pcbImageProcess.stderr.on('data', (data) => {
+  //   console.error(`stderr: ${data}`);
+  // });
   pcbImageProcess.stdin.setEncoding('utf8');
   process.on('exit', () => {
     pcbImageProcess.stdin.end();
   });
-  return async (pcbPath) => {
-    await spawnedPromise;
+  process.on('SIGINT', () => {
+    pcbImageProcess.stdin.end();
+  });
+  await spawnedPromise;
+  return pcbImageProcess;
+}
+
+function spawnPcbImageProcess() {
+  const processPromise = spawnDockerImage('pcb imager', 'yaqwsx/kikit:v1.3.0');
+  const imageGenerator = async (pcbPath) => {
+    const pcbImageProcess = await processPromise;
 
     const finishMessage = `done generating ${pcbPath} images`;
-    const imagePromise = new Promise(resolve => {
-      const listener = (data) => {
-        if (data.includes(finishMessage)) {
-          resolve();
-          pcbImageProcess.stdout.off('data', listener);
-        }
-      };
-      pcbImageProcess.stdout.on('data', listener);
-    });
+    const imagePromise = onceMessage(pcbImageProcess, finishMessage);
     console.log(`generating ${pcbPath} images`);
     pcbImageProcess.stdin.write(`${['front', 'back'].map(side => [
       '/usr/local/bin/pcbdraw',
@@ -121,102 +137,106 @@ function spawnPcbImageProcess() {
     await imagePromise;
     console.log(finishMessage);
   }
+
+  const gerberGenerator = async () => {
+    const converterProcess = await processPromise;
+    console.log('exporting gerbers');
+    const finishMessage = 'export gerbers done';
+    const onceMessagePromise = onceMessage(converterProcess, finishMessage);
+    converterProcess.stdin.write([
+      [
+        '/usr/local/bin/kikit',
+        'fab',
+        'jlcpcb',
+        '--no-drc',
+        '--no-assembly',
+        'output/routed_pcbs/board.kicad_pcb',
+        'output/gerbers/board'
+      ].join(' '),
+      `echo ${finishMessage}\n`,
+    ].join('; '));
+    await onceMessagePromise;
+    console.log('exported gerbers');
+  }
+
+  return [imageGenerator, gerberGenerator];
 }
 
 
-async function routePcb(pcbImage) {
-  await Promise.all([retry(convertKicadPcbToDSN, 'output/pcbs/board.dsn'), fs.mkdir('output/routed_pcbs', { recursive: true })]);
+async function routePcb() {
+  await Promise.all([retry(exportDsn, 'output/pcbs/board.dsn')]);
   await freeRouting();
-  await retry(reimportAsKicadPcb, 'output/routed_pcbs/board.kicad_pcb');
-  await Promise.all([gerbers(), fs.mkdir('output/gerbers', { recursive: true }), pcbImage('routed_pcbs/board')]);
+  await retry(importSes, 'output/routed_pcbs/board.kicad_pcb');
+  await Promise.all([generateGerber(), generatePcbImage('routed_pcbs/board')]);
 }
 
-async function convertKicadPcbToDSN() {
-  const docker = spawn('docker', [
-    'run',
-    '-w /board',
-    `-v ${__dirname}:/board`,
-    `-v ${__dirname}\\tmp:/tmp`,
-    '--rm',
-    'soundmonster/kicad-automation-scripts:latest',
-    '/bin/bash', '-c',
-    `"${[
-      'cp /board/export_dsn.py /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py',
+function spawnConvertKicadPcbToDSNProcess() {
+  const processPromise = spawnDockerImage('pcb converter', 'soundmonster/kicad-automation-scripts:latest')
+    .then(async converterProcess => {
+      const finishMessage = 'convert docker image ready';
+      const onceMessagePromise = onceMessage(converterProcess, finishMessage);
+      converterProcess.stdin.write([
+        'cp /board/import_ses.py /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py',
+        'cp /board/export_dsn.py /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py',
+        `echo ${finishMessage}\n`,
+      ].join('; '));
+      await onceMessagePromise;
+      return converterProcess;
+    });
+
+  const exportDsn = async () => {
+    const converterProcess = await processPromise;
+    console.log('exporting dsn');
+    const finishMessage = 'export dsn done';
+    const onceMessagePromise = onceMessage(converterProcess, finishMessage);
+    converterProcess.stdin.write([
       '/usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py output/pcbs/board.kicad_pcb output/pcbs/board.dsn', // add --record to record a video
-    ].join('; ')}"`,
-  ], { shell: true });
-  docker.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  docker.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  await once(docker, 'close');
-}
-// java -jar /opt/freerouting_cli.jar -de $< -do $@
-async function freeRouting() {
-  const docker = spawn('docker', [
-    'run',
-    '-w /board',
-    `-v ${__dirname}:/board`,
-    `-v ${__dirname}\\tmp:/tmp`,
-    '--rm',
-    'soundmonster/freerouting_cli:v0.1.0',
-    'java', '-jar', '/opt/freerouting_cli.jar',
-    '-de', 'output/pcbs/board.dsn',
-    '-do', 'output/routed_pcbs/board.ses',
-  ], { shell: true });
-  docker.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  docker.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  await once(docker, 'close');
-}
+      `echo ${finishMessage}\n`,
+    ].join('; '));
+    await onceMessagePromise;
+    console.log('exported dsn');
+  }
 
-async function reimportAsKicadPcb() {
-  const docker = spawn('docker', [
-    'run',
-    '-w /board',
-    `-v ${__dirname}:/board`,
-    `-v ${__dirname}\\tmp:/tmp`,
-    '--rm',
-    'soundmonster/kicad-automation-scripts:latest',
-    '/bin/bash', '-c',
-    `"${[
-      'cp /board/import_ses.py /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py',
+  const importSes = async () => {
+    const converterProcess = await processPromise;
+    console.log('importing ses');
+    const finishMessage = 'export dsn done';
+    const onceMessagePromise = onceMessage(converterProcess, finishMessage);
+    converterProcess.stdin.write([
       '/usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py --record output/pcbs/board.kicad_pcb output/routed_pcbs/board.ses --output-file output/routed_pcbs/board.kicad_pcb', // add --record to record a video
-    ].join('; ')}"`,
-  ], { shell: true });
-  docker.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  docker.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  await once(docker, 'close');
+      `echo ${finishMessage}\n`,
+    ].join('; '));
+    await onceMessagePromise;
+    console.log('imported ses');
+  }
+
+  return [exportDsn, importSes];
 }
 
-async function gerbers() {
-  const docker = spawn('docker', [
-    'run',
-    '-w /board',
-    `-v ${__dirname}:/board`,
-    '--rm',
-    'yaqwsx/kikit:v1.3.0',
-    'fab',
-    'jlcpcb',
-    '--no-drc',
-    '--no-assembly',
-    'output/routed_pcbs/board.kicad_pcb',
-    'output/gerbers/board'
-  ], { shell: true });
-  docker.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-  docker.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  await once(docker, 'close');
+function freeRoutingProcess() {
+  const processPromise = spawnDockerImage('free routing', 'soundmonster/freerouting_cli:v0.1.0')
+    .then(process => {
+      process.stdout.on('data', (data) => {
+        if (data.includes('FRCLI--PROGRESS')) {
+          console.log(`${data}`);
+        }
+      });
+    });
+
+  return async () => {
+    const routingProcess = await processPromise;
+    console.log('routing pcb');
+    const finishMessage = 'routing pcb done';
+    const onceMessagePromise = onceMessage(routingProcess, finishMessage);
+    routingProcess.stdin.write([
+      [
+        'java', '-jar', '/opt/freerouting_cli.jar',
+        '-de', 'output/pcbs/board.dsn',
+        '-do', 'output/routed_pcbs/board.ses',
+      ].join(' '),
+      `echo ${finishMessage}\n`,
+    ].join('; '));
+    await onceMessagePromise;
+    console.log('routed pcb');
+  }
 }
